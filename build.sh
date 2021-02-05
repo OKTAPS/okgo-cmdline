619cbb31cdb6eb5a1ddfde7f7e4eac7979660c4d
patluri001 <ramsai.atluri@gmail.com>
2021-02-05 18:26:50 -0500

    checking in all the binaries

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 go mod tidy
-go build -o okgo 
+env GOOS=linux GOARCH=amd64 go build -o ./build/linux-amd64/okgo
+env GOOS=linux GOARCH=386 go build -o ./build/linux-386/okgo
+env GOOS=linux GOARCH=arm go build -o ./build/linux-arm/okgo
+env GOOS=windows GOARCH=386 go build -o ./build/linux-386/okgo
+env GOOS=windows GOARCH=amd64 go build -o ./build/windows-amd64/okgo
+env GOOS=darwin GOARCH=amd64 go build -o ./build/darwin-amd64/okgo
+env GOOS=darwin GOARCH=386 go build -o ./build/darwin-386/okgo
